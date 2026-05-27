3edbc6cb5b5cce330127bff4202c15aec24869c9
Durgesh <durgesh.kumar@panopticinfotech.com>
2026-05-27 21:42:11 +0530

    Add function to reverse a string represented as an array

---
+++ array/reverseString.js
@@ -0,0 +1,16 @@
+
+function reverseString(s)  {
+    let i=0;
+    let j=s.length-1;
+    while(i<j){
+        let temp  = s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+
+    }
+    return s;
+};
+
+console.log(reverseString( ["h","e","l","l","o"]));
