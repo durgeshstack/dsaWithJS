e604cc096031043d7bf0e8840553d278419d8424
Durgesh <durgesh.kumar@panopticinfotech.com>
2026-05-28 22:25:53 +0530

    Add function to find the single number in an array where every element appears twice

---
+++ array/singleNumber.js
@@ -0,0 +1,13 @@
+/* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space. */
+
+
+function singleNumber(nums) {
+    let total = 0;
+    for(let i=0;i<nums.length;i++){
+       total = total^nums[i]
+    }
+
+    return total;
+};
