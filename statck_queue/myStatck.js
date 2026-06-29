cc4205a1b8fb9ffe6f32a4b058ee1724cec2178d
durgeshstack <durgeshksm2000@gmail.com>
2026-06-29 17:12:48 +0000

    stack using array

---
+++ statck_queue/myStatck.js
@@ -0,0 +1,18 @@
+function MyStack(){
+  this.stack=[];
+}
+
+MyStack.prototype.push = function(x){
+    this.stack.push(x);
+}
+
+MyStack.prototype.pop = function(){
+    return this.stack.pop();
+}
+MyStack.prototype.top = function(){
+    return this.stack[this.stack.length-1];
+}
+
+MyStack.prototype.empty = function(){
+    return this.stack.length===0;
+}
